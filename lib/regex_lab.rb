9afd2b228043f526fe1d9b43473796c838be336a
Sara Nolen <nolensara1@gmail.com>
2020-11-21 23:56:37 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,7 +3,7 @@ word.match(/\b[aeiou]/i) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-text.scan(/^[un]$[ing]+w/)
+text.grep(/^w[un]$[ing]+w/)
 end
 
 def words_five_letters_long(text)
