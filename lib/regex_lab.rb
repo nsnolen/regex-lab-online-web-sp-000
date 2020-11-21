24191eacdd55139cfed6f2439f0f45cd48a62248
Sara Nolen <nolensara1@gmail.com>
2020-11-21 23:57:02 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,7 +3,7 @@ word.match(/\b[aeiou]/i) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-text.grep(/^w[un]$[ing]+w/)
+text.grep(/^w[un]/)
 end
 
 def words_five_letters_long(text)
