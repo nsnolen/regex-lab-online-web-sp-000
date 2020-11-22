d746e75189fa8bb557765468034479bdf581d031
Sara Nolen <nolensara1@gmail.com>
2020-11-22 00:08:27 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,7 +3,7 @@ word.match(/\b[aeiou]/i) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-text.grep(/un\w+ing/)
+text.scan(/un\w+ing/)
 end
 
 def words_five_letters_long(text)
